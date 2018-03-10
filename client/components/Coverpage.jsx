c3086ebf82d41ded40466f96676fa808ff91323c
Maia <maiamiller25@gmail.com>
2018-03-10 16:15:50 +1300

    Footer now broken

--- client/components/Coverpage.jsx
+++ client/components/Coverpage.jsx
@@ -2,25 +2,30 @@ import React from 'react'
 
 export default (props) => {
 
+  // <h1 className='title is-1'>Hi, I'm Maia</h1>
+  // <h1 className='subtitle is 1'>I'm a software developer based in Wellington, NZ.</h1>
+  // <a href='#work' className='button is-medium is-danger'>My Work ></a>
+  // <a href='#contact' className='button is-medium is-danger'>Hire Me ></a>
 return (
-  <div id='coverpage-container'>
-      <div className='container'>
-        <div id='header-content-box'>
-          <div className='hero-body'>
-            <h1 className='title is-1'>Hi, I'm Maia</h1>
-            <h1 className='subtitle is 1'>I'm a software developer based in Wellington, NZ.</h1>
-            <a href='#work' className='button is-medium is-danger'>My Work ></a>
-            <a href='#contact' className='button is-medium is-danger'>Hire Me ></a>
-          </div>
-        </div>
 
-        <video loop muted autoPlay poster="img/videoframe.jpg" className="vid">
+  <div id='footer-container'>
+
+    <div id='content-box'>
+
+      <figure>
+        <img id='top' src="../images/acro.jpg" />
+      </figure>
+
+      <div id='vid-footer-container'>
+        <video autoPlay muted loop id="vid">
           <source src="../images/722741424.mp4" type="video/mp4" />
         </video>
-
       </div>
 
+    </div>
+
   </div>
+
 
   )
 }
--- client/components/Footer.jsx
+++ client/components/Footer.jsx
@@ -3,21 +3,17 @@ import React from 'react'
 export default (props) => {
 
   return (
-    <div id='footer-container'>
-
-      <div id='content-box'>
+    <div id='footer_container'>
 
         <figure>
-          <img id='top' src="../images/acro.jpg" />
+          <img id='footer_content' src="../images/acro.jpg" />
         </figure>
 
-        <div id='vid-footer-container'>
-          <video autoPlay muted loop id="vid">
+        <div id='vid_footer_container'>
+          <video autoPlay muted loop id="footer_vid">
             <source src="../images/722741424.mp4" type="video/mp4" />
           </video>
         </div>
-
-      </div>
 
     </div>
   )
