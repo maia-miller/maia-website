37958dbdf15c94fd9c498a042cfec5871eec402d
Maia <maiamiller25@gmail.com>
2018-03-22 13:06:06 +1300

    UJA infographic proj added

--- client/components/myWork/Projects/UJAinfographic.jsx
+++ client/components/myWork/Projects/UJAinfographic.jsx
@@ -3,18 +3,18 @@ import React from 'react'
 export default (props) => {
   return (
     <div className='has-text-centered'>
-      <h5 className='subtitle is-5'><strong>Announcement Email to Staff</strong></h5>
+      <h5 className='subtitle is-5'><strong>Infographic</strong></h5>
       <figure className='projimg'>
-          <a href="../images/UJAemail.jpg"><img className='tile-img' src="../images/UJAstaffemail.jpg" /></a>
+          <a href="../images/UJAinfographic.jpg"><img className='tile-img' src="../images/UJAinfographic.jpg" /></a>
         </figure>
-        <p className="description">I was given the requirement to design an email that was fun and engaging in order to announce the launch of UJA's Walk with Israel to staff. This email saw a 90% open rate and a 23% click-through rate. The majority of clicks (30%) came from the very bottom button, indicating that the design and content of the email encouraged users to read through the entirety of the email before engaging with the call to action.</p>
+        <p className="description">As part of the organization's Weekly Snapshot newsletter, I designed an image that could easily translate the value that the organization provided to the community. The objective was to convey real-world, concrete accomplishments in a quick and easy format. In addition to design skills, creating these infographics required the ability to interpret and transform often-cumbersome information into shareable and captivating visuals. This image was also shared on Facebook, Twitter and Instagram.</p>
         <div className='buttons is-centered'>
-          <span className="tag is-danger">Email Marketing</span>
           <span className="tag is-danger">Graphic Design</span>
+          <span className="tag is-danger">Communication</span>
         </div>
 
         <div className='buttons is-centered'>
-          <a href='../images/UJAemail.jpg' className='button is-primary is-outlined'>See the Whole Email</a>
+          <a href='../images/UJAinfographic.jpg' className='button is-primary is-outlined'>See the Whole Email</a>
         </div>
 
     </div>
