addfc0dc93bf71a48f5f8245afb1d09e0f36d1b2
Maia <maiamiller25@gmail.com>
2018-03-22 13:59:29 +1300

    DesignWork  done

--- client/components/myWork/DesignWork.jsx
+++ client/components/myWork/DesignWork.jsx
@@ -2,6 +2,8 @@ import React from 'react'
 
 import AnnouncementEmail from './Projects/AnnouncementEmail'
 import UJAinfographic from './Projects/UJAinfographic'
+import TASmag from './Projects/TASmag'
+import Banners from './Projects/Banners'
 
 export default (props) => {
     return (
@@ -14,6 +16,17 @@ export default (props) => {
             </article>
             <article className="tile is-child notification is-4" data-aos="flip-left" data-aos-duration="1000" data-aos-delay="200">
               <UJAinfographic />
+            </article>
+            <article className="tile is-child notification is-4" data-aos="flip-left" data-aos-duration="1000" data-aos-delay="200">
+              <TASmag />
+            </article>
+          </div>
+        </div>
+
+        <div className='tile is-ancestor'>
+          <div className="tile is-parent">
+            <article className="tile is-child notification is-4" data-aos="flip-left" data-aos-duration="1000" data-aos-delay="600">
+              <Banners />
             </article>
           </div>
         </div>
---
+++ client/components/myWork/Projects/Banners.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+export default (props) => {
+  return (
+    <div className='has-text-centered'>
+      <h5 className='subtitle is-5'><strong>Web Banners</strong></h5>
+      <figure className='projimg'>
+          <a href="../images/banners.jpg"><img className='tile-img' src="../images/banners.jpg" /></a>
+        </figure>
+        <p className="description">These select web banners were posted in rotation on the organization’s website to promote traffic towards event pages, and were consistent with the event's branding.</p>
+        <div className='buttons is-centered'>
+          <span className="tag is-danger">Graphic Design</span>
+        </div>
+
+        <div className='buttons is-centered'>
+          <a href='../images/banners.jpg' className='button is-primary is-outlined'>See the Ad</a>
+        </div>
+
+    </div>
+  )
+}
