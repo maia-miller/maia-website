c287ed0636a63f9c8f0c381c9a3adc6858156f6c
Maia <maiamiller25@gmail.com>
2018-04-10 21:42:22 +1200

    Changed images to purple blocks

--- client/components/About.jsx
+++ client/components/About.jsx
@@ -14,25 +14,29 @@ return (
 
     <div className='columns'>
       <div className='column'>
-        <img className='"col-figure image is-128x128 symbol" data-aos="zoom-in" data-aos-duration="600" data-aos-delay="200" col-img' src="../images/teamwork.png" />
+        <div className='purple-block'></div>
+        {/* <img className='"col-figure image is-128x128 symbol" data-aos="zoom-in" data-aos-duration="600" data-aos-delay="200" col-img' src="../images/teamwork.png" /> */}
         <h3 className='subject-title'><strong>Team player</strong></h3>
         <p className="description">The days of the reclusive programmer are over. Today, developers work on teams with designers, marketers, project managers and other programmers. With practical teamwork experience, it has been my strong interpersonal and communication skills that has contributed time and time again to my team's success.</p>
       </div>
 
       <div className='column'>
-        <img className='col-figure image is-128x128 symbol" data-aos="zoom-in" data-aos-duration="600" data-aos-delay="250" col-img' src="../images/problemsolve.png" />
+        <div className='purple-block'></div>
+        {/* <img className='col-figure image is-128x128 symbol" data-aos="zoom-in" data-aos-duration="600" data-aos-delay="250" col-img' src="../images/problemsolve.png" /> */}
         <h3 className='subject-title'><strong>Problem Solver</strong></h3>
         <p  className="description">I am an analytic person by nature, so my approach to any challenge is to investigate and optimize solutions. Nothing is impossible if turned over enough times, and I have a track record of providing clients with results that are professional, clean and creative. I love brainstorming solutions and solving problems, with a team or on my own.</p>
       </div>
 
       <div className='column'>
-        <img className='col-figure image is-128x128 symbol" data-aos="zoom-in" data-aos-duration="600" data-aos-delay="300" col-img' src="../images/fastlearner.png" />
+        <div className='purple-block'></div>
+        {/* <img className='col-figure image is-128x128 symbol" data-aos="zoom-in" data-aos-duration="600" data-aos-delay="300" col-img' src="../images/fastlearner.png" /> */}
         <h3 className='subject-title'><strong>Fast Learner</strong></h3>
         <p className="description">I learned how to code in just 18 weeks. Programmers need to be constantly learning in order to stay up-to-date with the field, and need to be able to do so quickly to stay relevant. I have a deep and insatiable curiosity for learning, and the self motivation and direction to put my intentions into action.</p>
       </div>
 
       <div className='column'>
-        <img className='col-figure image is-128x128 symbol" data-aos="zoom-in" data-aos-duration="600" data-aos-delay="350" col-img' src="../images/chart.png" />
+        <div className='purple-block'></div>
+        {/* <img className='col-figure image is-128x128 symbol" data-aos="zoom-in" data-aos-duration="600" data-aos-delay="350" col-img' src="../images/chart.png" /> */}
         <h3 className='subject-title'><strong>Multidisciplinary</strong></h3>
 
         <p className="description">With an education in Humanities, professional experience in marketing and graphic design, and training in Human-Computer Interactions, I bring a knowledge base that is both wide and deep. I think creatively and unconventionally, yet am grounded by best practices and an emphasis on accessibility and inclusivity.</p>
