bfe38f5bf296718da85d220a18c630f36f50bcd9
Maia <maiamiller25@gmail.com>
2018-04-11 18:21:55 +1200

    Testimonial box created, not mobile-friendly

--- client/components/Testimonial1.jsx
+++ client/components/Testimonial1.jsx
@@ -4,8 +4,8 @@ export default (props) => {
 
   return (
     <div className='testimonial'>
-      <h4 className='title is-4'>Positive. Succinct. Professional. Driven. If you are looking for an energetic breath of fresh air, Maia’s your person!</h4>
-      <h6 className='subtitle is-6'><em>— Deborah Jolly, Client, The Advocates’ Society</em></h6>
+      <h2 className='quote-title'>Positive. Succinct. Professional. Driven. If you are looking for an energetic breath of fresh air, Maia’s your person!</h2>
+      <h3 className='quote-subtitle'><em>— Deborah Jolly, Client, The Advocates’ Society</em></h3>
     </div>
   )
 }
