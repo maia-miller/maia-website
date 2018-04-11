73e9377850fa75afd679b3a585cfb5854b309cbb
Maia <maiamiller25@gmail.com>
2018-04-11 18:24:25 +1200

    Applied formatting to Testimonial2

--- client/components/Testimonial2.jsx
+++ client/components/Testimonial2.jsx
@@ -3,13 +3,9 @@ import React from 'react'
 export default (props) => {
 
   return (
-    <div className="notification is-warning is-narrow">
-      <div className='hero'>
-        <div className='container has-text-centered is-fullhd'>
-          <h4 className='title is-4'>“Outstanding” is the word that comes to mind. Maia establishes great relationships with her clients and constantly brings new ideas to the table. She would be an asset to any team!</h4>
-          <h6 className='subtitle is-6'><em>— Christina Grimaldi, Supervisor, The Cup Store</em></h6>
-        </div>
-      </div>
+    <div className='testimonial'>
+      <h2 className='quote-title'>“Outstanding” is the word that comes to mind. Maia establishes great relationships with her clients and constantly brings new ideas to the table. She would be an asset to any team!</h2>
+      <h3 className='quote-subtitle'><em>— Christina Grimaldi, Supervisor, The Cup Store</em></h3>
     </div>
   )
 }
