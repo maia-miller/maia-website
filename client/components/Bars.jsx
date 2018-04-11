523c9f68e1e4ad352254b72ccd584f9270426211
Maia <maiamiller25@gmail.com>
2018-04-11 17:31:05 +1200

    Bars are fixed in place

--- client/components/Bars.jsx
+++ client/components/Bars.jsx
@@ -4,7 +4,7 @@ export default (props) => {
 
   return (
     <div className='bars'>
-      <svg height='1000'>
+      <svg className='left-bars' height='1000' width='64'>
        <line x1={0} y1={35} x2={0} y2={800} stroke='#C39E37' strokeWidth={15} />
        <line x1={10} y1={35} x2={10} y2={800} stroke='#DDB41D' strokeWidth={7} />
 
@@ -15,7 +15,7 @@ export default (props) => {
        <line x1={61} y1={35} x2={61} y2={800} stroke='#DDB41D' strokeWidth={7} />
        </svg>
 
-       <svg height='1000'>
+       <svg className='right-bars' height='1000' width='84'>
         <line x1={0} y1={35} x2={0} y2={800} stroke='#C39E37' strokeWidth={15} />
         <line x1={10} y1={35} x2={10} y2={800} stroke='#DDB41D' strokeWidth={7} />
 
@@ -25,7 +25,7 @@ export default (props) => {
         <line x1={55} y1={35} x2={55} y2={800} stroke='#C39E37' strokeWidth={7} />
         <line x1={61} y1={35} x2={61} y2={800} stroke='#DDB41D' strokeWidth={7} />
         </svg>
-        
+
     </div>
 
   )
