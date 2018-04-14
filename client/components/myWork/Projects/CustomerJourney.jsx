4817f3e67390aa0fb6b3e6bfd67674b6f69cdaa5
Maia <maiamiller25@gmail.com>
2018-04-14 19:26:37 +1200

    CustomerJourney styled

--- client/components/myWork/Projects/CustomerJourney.jsx
+++ client/components/myWork/Projects/CustomerJourney.jsx
@@ -2,20 +2,21 @@ import React from 'react'
 
 export default (props) => {
   return (
-    <div className='has-text-centered'>
-      <h5 className='subtitle is-5'><strong>Customer Journey</strong></h5>
-        <figure className='projimg'>
-            <a href="../images/customerJourney.jpg"><img className='tile-img' src="../images/customerJourney.jpg" /></a>
-        </figure>
-        <p className="description">I mapped a workflow identifying all points of contact with the customer in order to identify pain points in the ordering process. This customer journey map enable the company to focus on areas of improvement to avoid redundancy and improve efficiency.</p>
-        <div className='buttons is-centered'>
-          <span className="tag is-danger">Strategy</span>
-          <span className="tag is-danger">User Experience</span>
-        </div>
+    <div className=''>
+      <h2 className='subject-title'>Customer Journey</h2>
+      <div className='img-container'>
+        <a href="../images/customerJourney.jpg"><img className='tile-img' src="../images/customerJourney.jpg" /></a>
+      </div>
+      <p className="description">I mapped a workflow identifying all points of contact with the customer in order to identify pain points in the ordering process. This customer journey map enable the company to focus on areas of improvement to avoid redundancy and improve efficiency.</p>
 
-        <div className='buttons is-centered'>
-          <a href='../images/customerJourney.jpg' className='button is-primary is-outlined'>View the Chart</a>
-        </div>
+      <div className='tags'>
+        <span className="tag">Strategy</span>
+        <span className="tag">User Experience</span>
+      </div>
+
+      <div className='demo'>
+        <a href='../images/customerJourney.jpg' className='demo-button'>View the Chart</a>
+      </div>
 
     </div>
   )
