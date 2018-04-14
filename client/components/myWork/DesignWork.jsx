25851f697971226c2fd25995fc8d95f4ea88d6e2
Maia <maiamiller25@gmail.com>
2018-04-14 18:57:34 +1200

    Styled PantryToPlate

--- client/components/myWork/DesignWork.jsx
+++ client/components/myWork/DesignWork.jsx
@@ -6,14 +6,14 @@ import TASmag from './Projects/TASmag'
 import Banners from './Projects/Banners'
 
 export default (props) => {
-    return (
-      <div id='work' className='work'>
-        <div className='project-tiles'>
-          <div className="project-tile" data-aos="flip-left" data-aos-duration="1000"><AnnouncementEmail /></div>
-          <div className="project-tile" data-aos="flip-left" data-aos-duration="1000" data-aos-delay="200"><UJAinfographic /></div>
-          <div className="project-tile" data-aos="flip-left" data-aos-duration="1000" data-aos-delay="200"><TASmag /></div>
-          <div className="project-tile" data-aos="flip-left" data-aos-duration="1000" data-aos-delay="600"><Banners /></div>
-        </div>
+  return (
+    <div id='work' className='work'>
+      <div className='project-tiles'>
+        <div className="project-tile" data-aos="flip-left" data-aos-duration="1000"><AnnouncementEmail /></div>
+        <div className="project-tile" data-aos="flip-left" data-aos-duration="1000" data-aos-delay="200"><UJAinfographic /></div>
+        <div className="project-tile" data-aos="flip-left" data-aos-duration="1000" data-aos-delay="400"><TASmag /></div>
+        <div className="project-tile" data-aos="flip-left" data-aos-duration="1000" data-aos-delay="200"><Banners /></div>
       </div>
-    )
+    </div>
+  )
 }
--- client/components/myWork/MarketingWork.jsx
+++ client/components/myWork/MarketingWork.jsx
@@ -7,15 +7,15 @@ import ArtAuction from './Projects/ArtAuction'
 import CustomerJourney from './Projects/CustomerJourney'
 
 export default (props) => {
-    return (
-      <div id='work' className='work'>
-        <a name="MarketingWork"></a>
-        <div className='project-tiles'>
-          <div className="project-tile" data-aos="flip-left" data-aos-duration="1000"><TCSMarketingPlan /></div>
-          <div className="project-tile" data-aos="flip-left" data-aos-duration="1000" data-aos-delay="200"><TCSsmResults /></div>
-          <div className="project-tile" data-aos="flip-left" data-aos-duration="1000" data-aos-delay="400"><ArtAuction /></div>
-          <div className="project-tile" data-aos="flip-left" data-aos-duration="1000" data-aos-delay="600"><CustomerJourney /></div>
-        </div>
+  return (
+    <div id='work' className='work'>
+      <a name="MarketingWork"></a>
+      <div className='project-tiles'>
+        <div className="project-tile" data-aos="flip-left" data-aos-duration="1000"><TCSMarketingPlan /></div>
+        <div className="project-tile" data-aos="flip-left" data-aos-duration="1000" data-aos-delay="200"><TCSsmResults /></div>
+        <div className="project-tile" data-aos="flip-left" data-aos-duration="1000" data-aos-delay="400"><ArtAuction /></div>
+        <div className="project-tile" data-aos="flip-left" data-aos-duration="1000" data-aos-delay="600"><CustomerJourney /></div>
       </div>
-    )
+    </div>
+  )
 }
