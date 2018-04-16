0f45ef5711e60c41e392f2cfd8d4e02a1ec79553
Maia <maiamiller25@gmail.com>
2018-04-16 22:19:26 +1200

    Hero banner complete

--- client/components/Coverpage.jsx
+++ client/components/Coverpage.jsx
@@ -6,21 +6,19 @@ return (
 
   <div className='coverpage'>
 
-    {/* <div id='coverpage_content'> */}
-      <span className="coverpage_text">
+      <div className="coverpage-text">
         <h1 className='hero-title'>Hi, I'm Maia</h1>
         <h2 className='hero-subtitle'>I'm a software developer based in Wellington, NZ.</h2>
         <button className="hero-button"><a className="hero-button" href='#work'>My Work</a></button>
         <button className="hero-button"><a className="hero-button" href='#contact'>Hire Me</a></button>
-      </span>
-    {/* </div> */}
+      </div>
 
 
-      {/* <div id='vid-coverpage-container'>
-        <video autoPlay muted loop id="vid">
+      <div id='vid-coverpage-container'>
+        <video autoPlay muted loop id="header-vid">
           <source src="../images/722741424.mp4" type="video/mp4" />
         </video>
-      </div> */}
+      </div>
 
 
   </div>
