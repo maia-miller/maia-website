ba3ecbfc5908f2c8ade3e993f8d24f462cba192e
Maia <maiamiller25@gmail.com>
2018-04-17 15:13:42 +1200

    Removed pointer-events from bars div to make buttons clickable

--- client/components/Bars.jsx
+++ client/components/Bars.jsx
@@ -4,18 +4,16 @@ export default (props) => {
 
   return (
     <div className='bars' >
-      <div className='left-bar-placement'>
-        <svg className='left-bars' height='1000' width='64'>
-         <line x1={0} y1={35} x2={0} y2={1000} stroke='#C39E37' strokeWidth={15} />
-         <line x1={10} y1={35} x2={10} y2={1000} stroke='#DDB41D' strokeWidth={7} />
+      <svg className='left-bars' height='1000' width='64'>
+       <line x1={0} y1={35} x2={0} y2={1000} stroke='#C39E37' strokeWidth={15} />
+       <line x1={10} y1={35} x2={10} y2={1000} stroke='#DDB41D' strokeWidth={7} />
 
-         <line x1={30} y1={0} x2={30} y2={1000} stroke='#C39E37' strokeWidth={7} />
-         <line x1={36} y1={0} x2={36} y2={1000} stroke='#DDB41D' strokeWidth={7} />
+       <line x1={30} y1={0} x2={30} y2={1000} stroke='#C39E37' strokeWidth={7} />
+       <line x1={36} y1={0} x2={36} y2={1000} stroke='#DDB41D' strokeWidth={7} />
 
-         <line x1={55} y1={35} x2={55} y2={1000} stroke='#C39E37' strokeWidth={7} />
-         <line x1={61} y1={35} x2={61} y2={1000} stroke='#DDB41D' strokeWidth={7} />
-         </svg>
-       </div>
+       <line x1={55} y1={35} x2={55} y2={1000} stroke='#C39E37' strokeWidth={7} />
+       <line x1={61} y1={35} x2={61} y2={1000} stroke='#DDB41D' strokeWidth={7} />
+       </svg>
 
        <div className='right-bar-placement'>
          <svg className='right-bars' height='1000' width='84'>
--- client/components/myWork/Projects/MakeMeAcro.jsx
+++ client/components/myWork/Projects/MakeMeAcro.jsx
@@ -5,7 +5,7 @@ export default (props) => {
     <div className=''>
       <h2 className='subject-title'>Make Me Acro</h2>
       <div className='img-container'>
-        <a href="http://makemeacro.herokuapps.com"><img className='tile-img' src="../images/makemeacro.png" /></a>
+        <a href="http://makemeacro.herokuapp.com"><img className='tile-img' src="../images/makemeacro.png" /></a>
       </div>
       <p className="tile-text">I spend lots of my time at acro jams, where you meet with people and improvise acro yoga poses. However, many times people end up standing around asking each other, "What pose should we do!?" Created in React, I built this app after 6 weeks of learning. It utilizes the whole stack, from a database to which users can contribute to an interactive interface.</p>
 
